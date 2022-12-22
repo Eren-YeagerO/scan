97e643049d094f0d13db80528830a0c154748fee
Levi <119001740+Shinigamiiz@users.noreply.github.com>
2022-12-22 20:24:00 +0530

    Update config.py

--- Scanner/config.py
+++ Scanner/config.py
@@ -1,4 +1,11 @@
 SUDO_USERS = "1143358497"
 OWNER_ID = 5667156680
 
+class Production(Config):
+    LOGGER = True
+
+
+class Development(Config):
+    LOGGER = True  # Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
+
 
