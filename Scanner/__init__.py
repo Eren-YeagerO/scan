43c85aee9c7e09c8f201a8bb41931402332e0e07
Sanjay Kumar <115239203+ErenYear@users.noreply.github.com>
2022-12-30 00:04:19 +0530

    Update __init__.py

--- Scanner/__init__.py
+++ Scanner/__init__.py
@@ -10,8 +10,6 @@ from Scanner.vars import API_HASH, API_ID, BOT_TOKEN, SESSION_STRING
 
 starttime = time.time()
 
-cleanmode = {}
-
 # enable logging
 FORMAT = "[Scanner] %(message)s"
 logging.basicConfig(
