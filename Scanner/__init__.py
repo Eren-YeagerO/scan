9a3f25ff9d29f484243db81c95ad0ac42e76b8b8
Sanjay Kumar <115239203+ErenYear@users.noreply.github.com>
2022-12-29 22:52:41 +0530

    Update __init__.py

--- Scanner/__init__.py
+++ Scanner/__init__.py
@@ -3,7 +3,6 @@ import time
 
 from aiohttp import ClientSession
 from pyrogram import Client
-from pyrogram.types import Message
 from telethon import TelegramClient
 from telethon.sessions import MemorySession
 
@@ -42,8 +41,6 @@ else:
     BOT_NAME = bot.first_name
 BOT_USERNAME = bot.username
 
-USERBOT_PREFIX = "."
-
 ub = ubot.get_me()
 ASS_ID = ub.id
 if ub.last_name:
@@ -53,13 +50,3 @@ else:
 ASS_USERNAME = ub.username
 
 tbot = TelegramClient(MemorySession(), API_ID, API_HASH)
-
-
-async def eor(msg: Message, **kwargs):
-    func = (
-        (msg.edit_text if msg.from_user.is_self else msg.reply)
-        if msg.from_user
-        else msg.reply
-    )
-    spec = getfullargspec(func.__wrapped__).args
-    return await func(**{k: v for k, v in kwargs.items() if k in spec})
