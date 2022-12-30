fc103fdd091b54665a712f7042fab8d03aa9d7bd
Sanjay Kumar <115239203+ErenYear@users.noreply.github.com>
2022-12-30 23:26:35 +0530

    Create chats_stats.py

---
+++ Scanner/plugins/chats_stats.py
@@ -0,0 +1,7 @@
+from Scanner import pbot as app
+from pyrogram import filters
+
+
+@app.on_message(filters.command("stat") & filters.group)
+async def stat(_, message):
+    await message.reply_text(f"Total Messages in{message.chat.title} :- {message.id}")
