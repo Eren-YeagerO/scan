99905cc3be9bd69303d84c6bc3fda3c22ca1c7d0
Sanjay Kumar <115239203+ErenYear@users.noreply.github.com>
2022-12-30 23:50:38 +0530

    Update chats_stats.py

--- Scanner/plugins/chats_stats.py
+++ Scanner/plugins/chats_stats.py
@@ -4,4 +4,4 @@ from pyrogram import filters
 
 @app.on_message(filters.command("stat") & filters.group)
 async def stat(_, message):
-    await message.reply_text(f"Total Messages in{message.chat.title} :- {message.id}")
+    await message.reply_text(f"**Total Messages in** **{message.chat.title}:-** {message.id}")
