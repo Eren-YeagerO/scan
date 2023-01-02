d731135bdde2b4824fbce4706c9f488c0a0b9359
Sanjay Kumar <115239203+ErenYear@users.noreply.github.com>
2023-01-02 19:45:50 +0530

    Update gban_assistant.py

--- Scanner/plugins/gban_assistant.py
+++ Scanner/plugins/gban_assistant.py
@@ -10,11 +10,11 @@ from Scanner.utils.filters import command
 from Scanner.db import global_bans_db as db
 
 def extract_gban(message):
-    hmmm = message.split("-id")[1]
-    hmm = hmmm.split("-r")  
+    hmmm = message.split(".id")[1]
+    hmm = hmmm.split(".r")  
     id = int(hmm[0].split()[0].strip())
-    reason = hmm[1].split("-p")[0].strip()
-    proof = hmm[1].split("-p")[1].strip()
+    reason = hmm[1].split(".p")[0].strip()
+    proof = hmm[1].split(".p")[1].strip()
     return id, reason, proof
 
 @ubot.on_message(command("zscan"))
@@ -30,7 +30,7 @@ async def scan(_, message: Message):
         await message.reply_text("id must be integer.")
         return
     except:
-        await message.reply_text("Format: `/zscan -id (id) -r (reason)  -p (proof link)`")
+        await message.reply_text("Format: `/zscan .id (id) .r (reason)  .p (proof link)`")
         return
     if int(user_id) in SUDO_USERS:
         await message.reply_text(
@@ -98,7 +98,7 @@ async def revert(_, message: Message):
         except ValueError:
             await message.reply_text("id must be integer.")
         except:
-            await message.reply_text("Format: `/zrevert -id (id)`")
+            await message.reply_text("Format: `/zrevert .id (id)`")
             return
     if not db.is_user_gbanned(user_id):
         await message.reply_text(f"User ID: {user_id} is not scanned.")
