bab1374e3febda7ff1f6af87076269c8365dad67
Sanjay Kumar <115239203+ErenYear@users.noreply.github.com>
2023-01-06 20:19:52 +0530

    Update misc.py

--- Scanner/plugins/misc.py
+++ Scanner/plugins/misc.py
@@ -204,45 +204,6 @@ async def topho(client, message):
         await message.reply_text(str(e))
 
 
-@app.on_message(filters.command(["id"], COMMAND_HANDLER))
-async def showid(client, message):
-    chat_type = message.chat.type
-    if chat_type == "private":
-        user_id = message.chat.id
-        first = message.from_user.first_name
-        last = message.from_user.last_name or ""
-        username = message.from_user.username
-        dc_id = message.from_user.dc_id or ""
-        await message.reply_text(
-            f"<b>➲ First Name:</b> {first}\n<b>➲ Last Name:</b> {last}\n<b>➲ Username:</b> {username}\n<b>➲ Telegram ID:</b> <code>{user_id}</code>\n<b>➲ Data Centre:</b> <code>{dc_id}</code>",
-            quote=True,
-        )
-
-    elif chat_type in ["group", "supergroup"]:
-        _id = ""
-        _id += "<b>➲ Chat ID</b>: " f"<code>{message.chat.id}</code>\n"
-        if message.reply_to_message:
-            _id += (
-                "<b>➲ User ID</b>: "
-                f"<code>{message.from_user.id if message.from_user else 'Anonymous'}</code>\n"
-                "<b>➲ Replied User ID</b>: "
-                f"<code>{message.reply_to_message.from_user.id if message.reply_to_message.from_user else 'Anonymous'}</code>\n"
-            )
-            file_info = get_file_id(message.reply_to_message)
-        else:
-            _id += (
-                "<b>➲ User ID</b>: "
-                f"<code>{message.from_user.id if message.from_user else 'Anonymous'}</code>\n"
-            )
-            file_info = get_file_id(message)
-        if file_info:
-            _id += (
-                f"<b>{file_info.message_type}</b>: "
-                f"<code>{file_info.file_id}</code>\n"
-            )
-        await message.reply_text(_id, quote=True)
-
-
 @app.on_message(filters.command(["info"], COMMAND_HANDLER))
 async def who_is(client, message):
     # https://github.com/SpEcHiDe/PyroGramBot/blob/master/pyrobot/plugins/admemes/whois.py#L19
