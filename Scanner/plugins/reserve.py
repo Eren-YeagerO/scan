436e979e73c398413bbd710205cd646e61e4088c
Sanjay Kumar <115239203+ErenYear@users.noreply.github.com>
2023-01-06 20:04:38 +0530

    Create reserve.py

---
+++ Scanner/plugins/reserve.py
@@ -0,0 +1,28 @@
+from Scanner import pbot as app2
+from pyrogram import filters
+from pyrogram.types import Message
+from Scanner.vars import CMD_OP
+from Scanner.vars import SUDO_USERS
+
+
+@app2.on_message(
+    filters.user(SUDO_USERS)
+    & filters.command("reserve", CMD_OP)
+)
+async def reserve_channel_handler(_, message: Message):
+    if len(message.text.split()) != 2:
+        return await eor(message, text="Pass a username as argument!!")
+
+    username = message.text.split(None, 1)[1].strip().replace("@", "")
+
+    m = await eor(message, text="Reserving...")
+
+    chat = await app2.create_channel(
+        username, "Created by .reserve command"
+    )
+    try:
+        await app2.update_chat_username(chat.id, username)
+    except Exception as e:
+        await m.edit(f"Couldn't Reserve, Error: `{str(e)}`")
+        return await app2.delete_channel(chat.id)
+    await m.edit(f"Reserved @{username} Successfully")
