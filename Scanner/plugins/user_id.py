d4c683c0bbe0ab89f8812545c7a3c545a0fc5b8a
Sanjay Kumar <115239203+ErenYear@users.noreply.github.com>
2023-01-07 22:53:06 +0530

    Update user_id.py

--- Scanner/plugins/user_id.py
+++ Scanner/plugins/user_id.py
@@ -9,11 +9,11 @@ from Scanner.eor import eor
 async def getid(client, message):
     chat = message.chat
     your_id = message.from_user.id
-    message_id = message.id
+    message_id = message.message_id
     reply = message.reply_to_message
 
-    text = f"[Message ID:]({message.link}) {message.id}\n"
-    text += f"[Your ID:](tg://user?id={your_id}) {your_id}\n"
+    text = f"**[Message ID:]({message.link})** `{message_id}`\n"
+    text += f"**[Your ID:](tg://user?id={your_id})** `{your_id}`\n"
 
     if not message.command:
         message.command = message.text.split()
@@ -22,17 +22,17 @@ async def getid(client, message):
         try:
             split = message.text.split(None, 1)[1].strip()
             user_id = (await client.get_users(split)).id
-            text += f"[User ID:](tg://user?id={user_id}) {user_id}\n"
+            text += f"**[User ID:](tg://user?id={user_id})** `{user_id}`\n"
         except Exception:
             return await eor(message, text="This user doesn't exist.")
 
-    text += f"[Chat ID:](https://t.me/{chat.username}) {chat.id}\n\n"
+    text += f"**[Chat ID:](https://t.me/{chat.username})** `{chat.id}`\n\n"
     if not getattr(reply, "empty", True):
         id_ = reply.from_user.id if reply.from_user else reply.sender_chat.id
         text += (
-            f"[Replied Message ID:]({reply.link}) {reply.message.id}\n"
+            f"**[Replied Message ID:]({reply.link})** `{reply.message_id}`\n"
         )
-        text += f"[Replied User ID:](tg://user?id={id_}) {id_}"
+        text += f"**[Replied User ID:](tg://user?id={id_})** `{id_}`"
 
     await eor(
         message,
